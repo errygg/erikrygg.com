17789666927caaa655770b843ec36b30a18920b4
Erik R. Rygg <errygg@gmail.com>
2018-06-30 16:34:57 -0600

    Fix editor issue

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -6,6 +6,7 @@ remote=$(git config remote.origin.url)
 
 git config --global user.email "$GH_EMAIL"
 git config --global user.name "$GH_NAME"
+git config --global core.editor "\usr\local\bin\subl -n -w"
 git checkout "$TARGET_BRANCH"
 git merge "$SOURCE_BRANCH"
 git add -A
