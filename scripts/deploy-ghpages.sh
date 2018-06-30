eac1150c3faecffc428bf0a77b4d8d7a0ff533a7
Erik R. Rygg <errygg@gmail.com>
2018-06-30 16:45:18 -0600

    Fixing deployment

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -7,9 +7,7 @@ remote=$(git config remote.origin.url)
 git config --global user.email "$GH_EMAIL"
 git config --global user.name "$GH_NAME"
 git checkout "$TARGET_BRANCH"
-git merge "$SOURCE_BRANCH"
-git add -A
-git commit -m "CircleCI Deployment" > /dev/null 2>&1
+git merge "$SOURCE_BRANCH" -m "CircleCI merging \'$SOURCE_BRANCH\' into \'$TARGET_BRANCH\'"
 git push origin "$TARGET_BRANCH"
 
 echo "Deployment complete!"
