dd2bd1fa704b514cc79461b6ba406c80ec99bd2f
Erik R. Rygg <errygg@gmail.com>
2018-06-30 16:38:12 -0600

    Fixing commit issue

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -6,11 +6,10 @@ remote=$(git config remote.origin.url)
 
 git config --global user.email "$GH_EMAIL"
 git config --global user.name "$GH_NAME"
-git config --global core.editor "\usr\local\bin\subl -n -w"
 git checkout "$TARGET_BRANCH"
 git merge "$SOURCE_BRANCH"
 git add -A
-git commit -m "CircleCI Deployment"
+git commit -m "CircleCI Deployment" > /dev/null 2>&1
 git push origin "$TARGET_BRANCH"
 
 echo "Deployment complete!"
