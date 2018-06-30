ca27e48af5d98c3283bda4a2b4f733345e4a0f42
Erik R. Rygg <errygg@gmail.com>
2018-06-30 16:31:55 -0600

    Update to deployment

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
-
 set -e
 
 pwd
 remote=$(git config remote.origin.url)
 
-mkdir gh-pages-branch
-cd gh-pages-branch
-
 git config --global user.email "$GH_EMAIL"
 git config --global user.name "$GH_NAME"
-git init
-git remote add --fetch origin "$remote"
-
-if git rev-parse --verify origin/gh-pages
-then
-  git checkout gh-pages
-  git rm -rf .
-else
-  git checkout --orphan gh-pages
-fi
-
+git checkout "$TARGET_BRANCH"
+git merge "$SOURCE_BRANCH"
 git add -A
-git commit --allow-empty -m "Deploy to GitHub pages"
-git push --force --quiet origin gh-pages
-cd ..
-rm -rf gh-pages-branch
+git commit -m "CircleCI Deployment"
+git push origin "$TARGET_BRANCH"
 
-echo "Finished Deployment!"
+echo "Deployment complete!"
