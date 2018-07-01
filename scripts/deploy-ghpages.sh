c2cb41a47f6f27edaf8eeb06ec62d1f863578602
Erik R. Rygg <errygg@gmail.com>
2018-06-30 22:21:51 -0600

    Update script to deploy static site pages

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -8,6 +8,8 @@ git config --global user.name "$GH_NAME"
 git config --global user.email "$GH_EMAIL"
 git checkout "$TARGET_BRANCH"
 git merge "$SOURCE_BRANCH" -m "CircleCI merging \'$SOURCE_BRANCH\' into \'$TARGET_BRANCH\'"
+bundle exec jekyll build
+git add --force -A
 git push origin "$TARGET_BRANCH"
 
 echo "Deployment complete!"
