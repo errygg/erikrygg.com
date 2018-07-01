cf95445b2a0dc47ea055b705c6b99a5466713907
Erik R. Rygg <errygg@gmail.com>
2018-06-30 22:33:05 -0600

    Adding some git commands

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -8,6 +8,8 @@ git config --global user.name "$GH_NAME"
 git config --global user.email "$GH_EMAIL"
 git checkout "$TARGET_BRANCH"
 git merge "$SOURCE_BRANCH" -m "CircleCI merging \'$SOURCE_BRANCH\' into \'$TARGET_BRANCH\'"
+git push origin "${TARGET_BRANCH}"
+git pull origin "${TARGET_BRANCH}"
 bundle install
 bundle exec jekyll build
 git add --force _site/
